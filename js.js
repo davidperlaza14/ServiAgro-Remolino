cb588639878e309e473a47a30780a1b09ad1523d
davidperlaza14 <davidperlaza14@gmail.com>
2021-08-24 06:56:02 -0500

    sitio-wed-Dana-Yajaira

---
+++ js.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.slider1').bxSlider({
+      mode: 'fade',
+    });
+    $('.slider2').bxSlider({
+      mode: 'fade',
+    });
+    $('.slider3').bxSlider({
+      mode: 'fade',
+    });
+    $('.slider4').bxSlider({
+        mode: 'fade',
+      });
+      $('.slider5').bxSlider({
+        mode: 'fade',
+      });
+      $('.slider6').bxSlider({
+        mode: 'fade',
+      });
+      $('.slider7').bxSlider({
+        mode: 'fade',
+      });
+      $('.slider8').bxSlider({
+        mode: 'fade',
+      });
+      $('.slider9').bxSlider({
+        mode: 'fade',
+      });
+      $('.slider10').bxSlider({
+        mode: 'fade',
+      });
+  });
